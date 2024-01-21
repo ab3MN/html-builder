8e65df14862d0221806c041a68ba991135b855fc
Mykhailo Nikolaiev <ab3mn3@gmail.com>
2024-01-21 18:47:31 +0200

    refactor:refactor 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,49 +1,23 @@
-const { exists, promises } = require('fs');
-const { mkdir, copyFile } = require('node:fs/promises');
+const { exists } = require('fs');
+const { mkdir, copyFile, readdir } = require('node:fs/promises');
 const { promisify } = require('util');
 const { join } = require('node:path');
 
 const _exists = promisify(exists);
 
-const getFilesList = async (dirName) => {
-  let files = [];
-
-  try {
-    const items = await promises.readdir(dirName, { withFileTypes: true });
-    for (const item of items) {
-      const pathFile = join(dirName, item.name);
-
-      item.isDirectory()
-        ? (files = [...files, ...(await getFilesList(pathFile))])
-        : files.push(item.name);
-    }
-  } catch (e) {
-    console.log(e);
-  }
-  return files;
-};
-
 const ORIGINAL__NAME = join(__dirname, 'files');
 const COPY__NAME = join(__dirname, 'copy-files');
 
-const isExist = async (name) => {
-  const _isExist = await _exists(name, (isExist) => isExist);
-  return _isExist;
-};
+const isExist = async (name) => await _exists(name, (isExist) => isExist);
 
-const copyDirectory = async () => {
+(async () => {
   const _isExist = await isExist(COPY__NAME);
 
   if (!_isExist) await mkdir(COPY__NAME);
 
-  const filesList = await getFilesList(ORIGINAL__NAME);
+  const filesList = await readdir(ORIGINAL__NAME, { withFileTypes: true });
 
-  filesList.forEach((file) => {
-    const originalName = join(ORIGINAL__NAME, file);
-    const copyName = join(COPY__NAME, file);
-
-    copyFile(originalName, copyName);
-  });
-};
-
-copyDirectory();
+  filesList.forEach(({ name }) =>
+    copyFile(join(ORIGINAL__NAME, name), join(COPY__NAME, name)),
+  );
+})();
