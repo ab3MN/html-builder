c4750e66ad8d67c1160471325b0566a2a21b90f6
Mykhailo Nikolaiev <ab3mn3@gmail.com>
2024-01-21 15:04:01 +0200

    refactor:rafactor 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,7 +2,9 @@ const { promises, stat } = require('fs');
 const path = require('path');
 const { stdout } = require('node:process');
 
-const getPath = (_path = '') => path.join(__dirname, `/${_path}`);
+const { promisify } = require('util');
+
+const _stat = promisify(stat);
 
 const getFilesList = async (dirName) => {
   let files = [];
@@ -11,25 +13,17 @@ const getFilesList = async (dirName) => {
     for (const item of items) {
       const pathFile = path.join(dirName, item.name);
 
-      if (!item.isDirectory()) {
+      if (item.isDirectory()) {
+        files = [...files, ...(await getFilesList(pathFile))];
+      } else {
         const { name, ext } = path.parse(pathFile);
 
         if (name && ext) {
-          let fileInfo = name + ' - ' + ext.slice(1);
+          const stats = await _stat(pathFile);
+          const size = (stats.size / 1024).toFixed(3) + 'kb';
 
-          stat(pathFile, (err, stats) => {
-            try {
-              const size = (stats.size / 1024).toFixed(3) + 'kb';
-              stdout.write(fileInfo + ' - ' + size + '\n');
-            } catch {
-              return err.message;
-            }
-          });
-
-          files.push(fileInfo);
+          files.push(name + ' - ' + ext.slice(1) + ' - ' + size);
         }
-      } else {
-        files = [...files, ...(await getFilesList(pathFile))];
       }
     }
   } catch (e) {
@@ -38,6 +32,6 @@ const getFilesList = async (dirName) => {
   return files;
 };
 
-const folderPath = getPath('secret-folder');
-
-getFilesList(folderPath);
+getFilesList(path.join(__dirname, '/secret-folder'))
+  .then((files) => files.forEach((file) => stdout.write(file + '\n')))
+  .catch((e) => console.log(e));
